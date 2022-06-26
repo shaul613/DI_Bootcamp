19af04076ffebf10e6c9538ff4e7c4cc94cde6c2
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-06-26 05:49:48 -0400

    did class exer 3

--- week_2/day_1/ExercisesXP/script.js
+++ week_2/day_1/ExercisesXP/script.js
@@ -5,6 +5,15 @@ let favoriteMeal = "Dinner";
 console.log("I will eat " + favoriteFood + " for " + favoriteMeal);
 
 // Exercise 2
+// Using this array : let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+//
+// Create a variable named myWatchedSeriesLength that is equal to the number of series in the myWatchedSeries array.
+//
+// Create a variable named myWatchedSeriesSentence, that is equal to a sentence describing the series you watched
+// For example : black mirror, money heist, and the big bang theory
+//
+// Console.log a sentence using both of the variables created above
+// For example : I watched 3 series : black mirror, money heist, and the big bang theory
 let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
 let myWatchedSeriesLength = myWatchedSeries.length;
 //console.log(myWatchedSeries[0]);
