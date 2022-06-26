23afa4bdcc8671c756d0a9e6b8c1a386b2a1cbd9
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-06-26 06:24:54 -0400

    did exer 3

--- week_2/day_1/ExercisesXP/script.js
+++ week_2/day_1/ExercisesXP/script.js
@@ -34,10 +34,12 @@ console.log(myWatchedSeries);
 
 //Exercise 3
 // Store a celsius temperature into a variable.
-// 
+let temperature = 100;
 // Convert it to fahrenheit and console.log <temperature>°C is <temperature>°F.
 // Hint : Should you create another variable to hold the temperature in fahrenheit? (ie. point 2)
 // Hint: To convert a temperature from celsius to fahrenheit : Divide it by 5, then multiply it by 9, then add 32
+let celsiusToFahrenheit = temperature * 9 / 5 + 32;
+console.log(`${temperature} C is ${celsiusToFahrenheit} F.`);
 //Exercise 4
 
 //Exercise 5
