1630a805cae0f8aacb0f828b00174ff02f7e5913
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-06-26 03:10:25 -0400

    added exercise 1

---
+++ week_2/day_1/ExercisesXP/script.js
@@ -0,0 +1,15 @@
+
+// Exercise 1
+let favoriteFood = "Pizza";
+let favoriteMeal = "Dinner";
+console.log("I will eat " + favoriteFood + " for " + favoriteMeal);
+
+// Exercise 2
+
+//Exercise 3
+
+//Exercise 4
+
+//Exercise 5
+
+//Exercise 6
