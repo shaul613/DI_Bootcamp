0f9a2e85802e0bd4a7926e19385fc2c20f2531de
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-06-26 03:23:40 -0400

    start exer2

--- week_2/day_1/ExercisesXP/script.js
+++ week_2/day_1/ExercisesXP/script.js
@@ -5,6 +5,10 @@ let favoriteMeal = "Dinner";
 console.log("I will eat " + favoriteFood + " for " + favoriteMeal);
 
 // Exercise 2
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length;
+//console.log(myWatchedSeries[0]);
+let myWatchedSeriesSentence = myWatchedSeries[0];
 
 //Exercise 3
 
