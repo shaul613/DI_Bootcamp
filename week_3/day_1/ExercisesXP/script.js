a54d312e55224717c6368f040535e37b7c35656c
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-07-03 05:35:40 -0400

    practiced DOM

---
+++ week_3/day_1/ExercisesXP/script.js
@@ -0,0 +1,9 @@
+// Retrieve the div and console.log it
+console.log(document.getElementsByTagName("div"));
+// Change the name “Pete” to “Richard”.
+window.onload = function(){
+  document.textContent("Pete").innerHTML = "Richard";
+}
+
+// Change each first name of the two <ul>'s to your name.
+// Delete the <li> that contains the text node “Sarah”.
