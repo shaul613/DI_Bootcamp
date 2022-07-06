0eafc6e6ea1f7bcb6ca96e98d6901369e673611f
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-07-06 08:18:39 -0400

    finished html and css for mp

--- week_3/day_4/MP/colors/script.js
+++ week_3/day_4/MP/colors/script.js
@@ -13,5 +13,13 @@ function addColors(){
 addColors();
 
 function addBoxes(){
-
+  for(i=0; i<1024; i++){
+    let elm = document.createElement("div");
+    // let text = document.createTextNode(i);
+    elm.classList.add("fillBox");
+    // elm.appendChild(text);
+    document.getElementById("boxes").appendChild(elm);
+  }
 }
+
+addBoxes();
