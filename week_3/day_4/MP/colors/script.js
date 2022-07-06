05ebca61dc2146f667ef8c2bc963476c78bcc683
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-07-06 08:25:09 -0400

    added colors

--- week_3/day_4/MP/colors/script.js
+++ week_3/day_4/MP/colors/script.js
@@ -1,5 +1,5 @@
 
-let colorsArr = "red blue green yellow".split(" ");
+let colorsArr = "red blue green yellow #2282d9 #6aab55 #e54e98 #d59631 #77bd6a #e9e636 #cfbb95 #ce6dde #2c673e #573918 #cdbba7 #11ddee #d8585866 #aab52966 #25894466 #4468b966 #38150c66".split(" ");
 
 function addColors(){
   for(i in colorsArr){
