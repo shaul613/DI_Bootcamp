89c1108da1e1d60eeaf624d15231c1b8c64f0a80
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-07-24 06:12:54 -0400

    started exercise xp1

---
+++ week_6/day_1/exercisesXP/exerciseXP_1/file_2.sql
@@ -0,0 +1,8 @@
+-- All the items.
+SELECT * FROM items;
+-- All the items with a price above 80 (80 not included).
+SELECT * FROM items WHERE price > 80;
+-- All the items with a price below 300. (300 included)
+-- All customers whose last name is ‘Smith’ (What will be your outcome?).
+-- All customers whose last name is ‘Jones’.
+-- All customers whose firstname is not ‘Scott’.
