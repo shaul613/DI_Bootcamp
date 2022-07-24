1e568d0db8fa33f463abae9391024828dad9b11c
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-07-24 05:02:06 -0400

    did class exercise

---
+++ week_6/day_1/practice_class_exercises/file1.sql
@@ -0,0 +1,12 @@
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Amy','Smith','07/01/1928', 4),;
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Darcy','Anderson','11/09/1984', 4);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Amy','Smith','07/01/1928', 4),
+('Nicole','Smith','07/01/1928', 4),
+('Emily','Smith','07/01/1928', 4);
+
+select * from actors;
