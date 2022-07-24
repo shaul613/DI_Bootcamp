f6b7b4c4972df40e206b640ebde7a2784988978b
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-07-24 13:27:41 -0400

    did dc

---
+++ week_6/day_1/daily_challenge/dc.sql
@@ -0,0 +1,4 @@
+-- 1. Count how many actors are in the table.
+SELECT * FROM actors;
+-- 2. Try to add a new actor with some blank fields. What do you think the outcome will be ?
+-- I WILL GET AN ERROR BECASUE THEY ARE MARKED AS 'NOT NULL'!
